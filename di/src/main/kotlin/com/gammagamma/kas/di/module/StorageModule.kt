0af718c5e965ba83dbd9b7f0bac896103d311556
Wesley Bliss <wesley.bliss@gmail.com>
2020-04-17 03:11:34 -0400

    Initial SQLDelight setup

--- di/src/main/kotlin/com/gammagamma/kas/di/module/StorageModule.kt
+++ di/src/main/kotlin/com/gammagamma/kas/di/module/StorageModule.kt
@@ -1,11 +1,17 @@
 package com.gammagamma.kas.di.module
 
+import com.gammagamma.kas.Database
 import com.gammagamma.kas.domain.storage.IStorageProvider
 import com.gammagamma.kas.storage.HawkStorageProvider
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import com.squareup.sqldelight.db.SqlDriver
 import org.koin.dsl.module
 
 val StorageModule = module {
     
     single<IStorageProvider> { HawkStorageProvider() }
+    
+    single<SqlDriver> { AndroidSqliteDriver(Database.Schema, get(), "kas.db") }
+    single { Database(get()) }
     
 }
